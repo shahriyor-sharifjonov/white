7623524bfe1b38597be16c40d35058ff8d8ae515
Shahriyor <shahriyordeveloping@gmail.com>
2021-09-10 14:29:45 +0300

    intial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -98,3 +98,55 @@ const menuToggle2 = () => {
 };
 
 headerButton2.onclick = menuToggle2;
+
+
+if(document.querySelector('.carousel')){
+  $('.carousel').slick({
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    touchMove: false,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false,
+        }
+      },
+    ]
+  });
+}
+
+
+let windowLess = false
+function checkWindowSize(){
+  if(window.innerWidth < 1400){
+    windowLess = true
+  }else{
+    windowLess = false
+  }
+}
+if(document.getElementsByClassName('custom-container')){
+  var mustRemove = document.getElementsByClassName('custom-container');
+}
+function checkWindow(){
+  checkWindowSize()
+  if(windowLess){
+    for(let i = 0; i < mustRemove.length; i++){
+      mustRemove[i].classList.remove('container-sm')
+      mustRemove[i].classList.add('container')
+    }
+  }else{
+    for(let i = 0; i < mustRemove.length; i++){
+      mustRemove[i].classList.add('container-sm')
+      mustRemove[i].classList.remove('container')
+    }
+  }
+}
+window.onload = checkWindow
+window.addEventListener('resize', function(){
+  checkWindow()
+})
